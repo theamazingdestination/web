1df2918324387eff8ceb0ca933fbf83e037cfb15
theamazingdestination <55959656+theamazingdestination@users.noreply.github.com>
2020-05-16 02:18:02 +0530

    Add files via upload

---
+++ new/assests/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+
+    // var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    // var today = new Date();
+    // //today.toLocaleDateString("en-US", options)
+    // //today.toLocaleDateString("hi-IN", options)
+    // $(".today_date").html(today.toLocaleDateString("en-US", options))
+});
